215060b1657e571d8ca04361c16900fa0966047a
makyv <86384334+makyv@users.noreply.github.com>
2021-09-25 20:28:14 +0200

    Update Circle.java

--- week5/Wednesday/Circle.java
+++ week5/Wednesday/Circle.java
@@ -1,1 +1,32 @@
+package Domaci_sreda;
 
+public class Circle extends Shape {
+    private double r;
+
+    public Circle(String name, double r) {
+        super(name);
+        this.r = r;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+
+    @Override
+    public double area() {
+        double P = Math.PI * (Math.pow(r, 2));
+        return P;
+    }
+
+    @Override
+    public double circumference() {
+        double O = 2 * r * Math.PI;
+        return O;
+    }
+
+}
